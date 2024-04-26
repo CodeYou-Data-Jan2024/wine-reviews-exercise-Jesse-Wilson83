d282c4bf3b095c7c453067630132f514e5eaab2c
Jesse Wilson <staypuff64@gmail.com>
2024-04-26 17:59:03 -0400

    Added code and file

--- reviews.py
+++ reviews.py
@@ -1,2 +1,18 @@
-# add your code here
+import pandas as pd
+reviews = pd.read_csv("C:\\Users\\stayp\\github-classroom\\CodeYou-Data-Jan2024\\wine-reviews-exercise-Jesse-Wilson83\\data\\winemag-data-130k-v2.csv.zip")
 
+#get the median
+median_points = reviews.groupby('country').points.mean().round(1)
+
+#get the countries
+countries = reviews.country.unique()
+
+# get the number of reviews
+reviews_per_country = reviews.country.value_counts()
+
+#create new data file
+review_df = pd.DataFrame({'country':countries,'count':reviews_per_country.reindex(countries),'points':median_points.reindex(countries)})
+review_df.fillna(0, inplace=True)
+review_df['count']=review_df['count'].astype(int)
+review_df_sorted = review_df.sort_values(by='count', ascending=False)
+review_df_sorted.to_csv('C:\\Users\\stayp\\github-classroom\\CodeYou-Data-Jan2024\\wine-reviews-exercise-Jesse-Wilson83\\data\\reviews_per_country.csv', index=False, sep=',')
